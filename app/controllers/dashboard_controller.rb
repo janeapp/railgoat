cc1e43a0b5254e7cc2bc3ba2d75f208877877659
Mohammad Kamrani <mohammad.kamrani@jane.app>
2026-05-01 09:12:26 -0700

    fix: remove dynamic method dispatch in change_graph action

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -13,8 +13,6 @@ class DashboardController < ApplicationController
   end
 
   def change_graph
-    self.try(params[:graph])
-
     if params[:graph] == "bar_graph"
       render "dashboard/bar_graph"
     else
@@ -23,3 +21,5 @@ class DashboardController < ApplicationController
     end
   end
 end
+
+
